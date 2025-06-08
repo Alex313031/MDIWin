74e1e2b9100bdae04e28ffbe2a0b27de078d391e
Alexander Frick <Alex313031@gmail.com>
2025-06-08 18:23:07 -0500

    move gblobals to header, convert tabs to spaces

--- app_four.h
+++ app_four.h
@@ -3,6 +3,20 @@
 #ifndef APP_FOUR_H_
 #define APP_FOUR_H_
 
+#include "resource.h"
 
+const char g_szClassName[] = "myWindowClass";
+const char g_szChildClassName[] = "myMDIChildWindowClass";
+
+#define IDC_MAIN_MDI	101
+#define IDC_MAIN_TOOL	102
+#define IDC_MAIN_STATUS	103
+
+#define IDC_CHILD_EDIT	101
+
+#define ID_MDI_FIRSTCHILD 50000
+
+HWND g_hMDIClient = NULL;
+HWND g_hMainWindow = NULL;
 
 #endif  // APP_FOUR_H_
